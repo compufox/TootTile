e3cda26ce9e7186e135e9ba507c4dcfb2357c207
Epps <zepps@wpccs-imac.wpcc.edu>
2017-09-11 14:53:02 -0400

    first push

---
+++ app/src/main/java/thezacattacks/toottile/TootTileService.java
@@ -0,0 +1,49 @@
+package thezacattacks.toottile;
+
+import android.content.Intent;
+import android.service.quicksettings.TileService;
+
+import static android.support.v4.content.ContextCompat.startActivity;
+
+/**
+ * Created by zepps on 9/11/17.
+ */
+
+public class TootTileService extends TileService {
+
+    public TootTileService() {
+        super();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+       // startActivity(new Intent(this, )); TODO: Make compose class
+    }
+
+}
