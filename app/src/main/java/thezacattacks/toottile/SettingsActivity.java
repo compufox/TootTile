c692f3cabe7a7ac384de7ffb72cbf6f0e756caaf
Zac <zactepps@gmail.com>
2017-09-14 16:53:54 -0400

    back button in settings works now also removed import from main

--- app/src/main/java/thezacattacks/toottile/SettingsActivity.java
+++ app/src/main/java/thezacattacks/toottile/SettingsActivity.java
@@ -133,6 +133,17 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * {@inheritDoc}
      */
